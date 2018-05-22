a443fb204772c96a2058dab2defccff9d5295d5b
Linda Lawton <lilaw@eg.dk>
2018-05-22 09:03:05 +0200

    Making setup easer.

--- src/JavaScriptClient/wwwroot/app.js
+++ src/JavaScriptClient/wwwroot/app.js
@@ -1,4 +1,11 @@
-﻿function log() {
+﻿var authority = "https://login.xena.biz";
+var client_id = "41eb10a8-7d7a-47ed-9cc4-8e1bfae2df25.xena.biz";
+var redirect_uri = "http://localhost:5003/callback.html";
+var post_logout_redirect_uri = "http://localhost:5003/index.html";
+var scopes = "openid profile testapi";
+var XenaAPIEndpoint = "https://www.xena.biz";
+
+function log() {
     document.getElementById("results").innerText = "";
 
     Array.prototype.forEach.call(arguments, function (msg) {
@@ -17,12 +24,12 @@ document.getElementById("api").addEventListener("click", api, false);
 document.getElementById("logout").addEventListener("click", logout, false);
 
 var config = {
-    authority: "https://login.xena.biz",
-    client_id: "[Set Your Client Id here]",
-    redirect_uri: "http://localhost:5003/callback.html",
+    authority: authority,
+    client_id: client_id,
+    redirect_uri: redirect_uri,
     response_type: "id_token token",
-    scope: "openid profile testapi",
-    post_logout_redirect_uri: "http://localhost:5003/index.html",
+    scope: scopes,
+    post_logout_redirect_uri: post_logout_redirect_uri
 };
 var mgr = new Oidc.UserManager(config);
 
@@ -41,7 +48,7 @@ function login() {
 
 function api() {
     mgr.getUser().then(function (user) {
-        var url = "https://login.xena.biz/connect/userinfo";
+        var url = XenaAPIEndpoint + "/Api/User/XenaUserMembership?ForceNoPaging=true";
 
         var xhr = new XMLHttpRequest();
         xhr.open("GET", url);
